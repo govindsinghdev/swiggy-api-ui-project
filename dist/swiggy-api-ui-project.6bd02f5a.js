516fe000a1bbf72fba40a7af82d69bc8b9851f6c
govindsinghdev <govindsinghdev10@gmail.com>
2026-04-14 04:23:04 +0530

    same

--- dist/swiggy-api-ui-project.6bd02f5a.js
+++
@@ -1,7 +0,0 @@
-const parent = React.createElement("h1", {
-    class: "heading"
-}, "Hello React API Implementation!");
-const root = ReactDOM.reacteRoot(document.getElementById("root"));
-root.render(parent);
-
-//# sourceMappingURL=swiggy-api-ui-project.6bd02f5a.js.map
